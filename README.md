128f83e57beb49967f1a3d2a179d65be8c0e9c24
Ashik Nesin <mail@ashiknesin.com>
2018-06-17 19:55:34 +0530

    Add README & daysUntilDebtFree fn

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
+> Finanza 💰
+
+Simple library to perform common personal financial operations
+
+## Install
+
+```
+$ npm install finanza
+```
+
+## Usage
+
 ### calculateMonthlyPayment
 
 ```js
+const monthlyPayment = calculateMonthlyPayment({
+  principal: 15812.0,
+  monthlyInterestInPercentage: 1.17,
+  noOfMonths: 9
+});
+
+console.log(monthlyPayment); //  1861.2606240059595
 ```
+
+### calculateAmortization
+
+```js
+const AM = calculateAmortization({
+  principal: 15812.0,
+  monthlyPayment: 1860.96,
+  monthlyInterestInPercentage: 1.17,
+  firstPaymentDate: "2018-06-15",
+  noOfMonths: 9
+});
+
+/* console.log(AM) 👇
+[ { currentInterest: 185.00039999999998,​​​​​
+​​​​​    currentPrinciple: 1675.9596000000001,​​​​​
+​​​​​    endingBalance: 14136.0404,​​​​​
+​​​​​    currentPaymentDate: Fri Jun 15 2018 00:00:00 GMT+0530 (IST),​​​​​
+​​​​​    pastDate: true },​​​​​
+...
+​​​​​  { currentInterest: 21.554225914525635,​​​​​
+​​​​​    currentPrinciple: 1839.4057740854744,​​​​​
+​​​​​    endingBalance: 2.835757070562977,​​​​​
+​​​​​    currentPaymentDate: Fri Feb 15 2019 00:00:00 GMT+0530 (IST),​​​​​
+​​​​​    pastDate: false } ]​​​​​
+
+*/
+```
+
+**Note**
+Month starts with 0-index in js. Make sure you pass proper month.
+
+### daysUntilDebtFree
+
+```js
+daysUntilDebtFree({ amortizationList: AM }); // 242
+```
+
+## License
+
+MIT © [Ashik Nesin](https://ashiknesin.com)
--- index.js
+++ index.js
@@ -8,17 +8,82 @@ const percentToDecimal = percentage => percentage / 100;
 
 // APIs
 const calculateMonthlyPayment = ({
-	principal = 0,
-	monthlyInterestInPercentage = 0,
-	noOfMonths = 0
+  principal = 0,
+  monthlyInterestInPercentage = 0,
+  noOfMonths = 0
 }) => {
-	const PRINCIPAL = principal;
-	const MONTHLY_INTEREST_DECIMAL = percentToDecimal(
-		monthlyInterestInPercentage
-	);
-	const NO_OF_MONTHS = noOfMonths;
-	const numerator = PRINCIPAL * MONTHLY_INTEREST_DECIMAL;
-	const denominator = 1 - Math.pow(1 + MONTHLY_INTEREST_DECIMAL, -NO_OF_MONTHS);
-	const monthlyPaymentAmount = numerator / denominator;
-	return monthlyPaymentAmount;
+  const PRINCIPAL = principal;
+  const MONTHLY_INTEREST_DECIMAL = percentToDecimal(
+    monthlyInterestInPercentage
+  );
+  const NO_OF_MONTHS = noOfMonths;
+  const numerator = PRINCIPAL * MONTHLY_INTEREST_DECIMAL;
+  const denominator = 1 - Math.pow(1 + MONTHLY_INTEREST_DECIMAL, -NO_OF_MONTHS);
+  const monthlyPaymentAmount = numerator / denominator;
+  return monthlyPaymentAmount;
+};
+
+const calculateAmortization = ({
+  principal = 0,
+  monthlyInterestInPercentage = 0,
+  noOfMonths = 0,
+  monthlyPayment = 0,
+  firstPaymentDate = null
+}) => {
+  const MONTHLY_PAYMENT = monthlyPayment;
+  const MONTHLY_INTEREST_DECIMAL = percentToDecimal(
+    monthlyInterestInPercentage
+  );
+  let FIRST_PAYMENT_DATE = null;
+  //? Make firstPaymentDate required?
+  if (dayjs(firstPaymentDate).isValid()) {
+    FIRST_PAYMENT_DATE = dayjs(firstPaymentDate); // Since date starts with 0-index in js
+  }
+
+  // Let's calculate ⚡️
+  const schedule = [];
+
+  let startingBalance = principal;
+  let currentPaymentDate = FIRST_PAYMENT_DATE;
+
+  for (let i = 0; i < noOfMonths; i++) {
+    const item = {};
+    item.currentInterest = startingBalance * MONTHLY_INTEREST_DECIMAL;
+    item.currentPrinciple = MONTHLY_PAYMENT - item.currentInterest;
+    item.endingBalance = startingBalance - item.currentPrinciple;
+
+    if (dayjs(currentPaymentDate).isValid()) {
+      item.currentPaymentDate = currentPaymentDate.toDate();
+      item.pastDate = dayjs(currentPaymentDate).isBefore(dayjs());
+    }
+
+    schedule.push(item);
+
+    startingBalance = item.endingBalance;
+
+    if (dayjs(currentPaymentDate).isValid()) {
+      currentPaymentDate = currentPaymentDate.add("1", "month");
+    }
+  }
+  return schedule;
+};
+
+const daysUntilDebtFree = ({ amortizationList = [] }) => {
+  //TODO Are those validations are right?
+  if (!amortizationList.length) {
+    throw new Error("amortizationList");
+  }
+  if (!amortizationList[0].currentPaymentDate) {
+    throw new Error("amortizationList item must have payment date");
+  }
+
+  const lastPayment =
+    amortizationList[amortizationList.length - 1].currentPaymentDate;
+  return dayjs(lastPayment).diff(dayjs(), "days");
+};
+
+module.exports = {
+  calculateMonthlyPayment,
+  calculateAmortization,
+  daysUntilDebtFree
 };
